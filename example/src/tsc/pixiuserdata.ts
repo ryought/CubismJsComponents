f0ade68883b9ed40bd9326c9a53c740aeac6ae57
shiozawa-at-live2d-com <shiozawa@live2d.com>
2017-12-11 15:47:52 +0900

    Add user data example.

--- example/src/tsc/pixiuserdata.ts
+++ example/src/tsc/pixiuserdata.ts
@@ -4,13 +4,13 @@
  * Use of this source code is governed by the Live2D Open Software license
  * that can be found at http://live2d.com/eula/live2d-open-software-license-agreement_en.html.
  */
-namespace piximultiplemodel
- {
+
+namespace pixiuserdata{
 
 // Properties.
 let app: PIXI.Application;
 let koharu: LIVE2DCUBISMPIXI.Model;
-let mark: LIVE2DCUBISMPIXI.Model;
+let message: PIXI.Text;
 
 // Keep 16:9 ratio.
 let width = window.innerWidth;
@@ -22,8 +22,11 @@ viewMain();
 // Load and add cubism model.
 loadAssets()
 .then(addModel)
+.then(showMessageWindow)
 .catch(loadFail);
 
+// Show message window and text.
+//showMessageWindow();
 
 function viewMain(){
     // Create app.
@@ -33,11 +36,11 @@ function viewMain(){
     document.body.appendChild(app.view);
 
     // Set background image
-    var bgTexture = PIXI.Texture.fromImage('../assets/background.png');
+    let bgTexture = PIXI.Texture.fromImage('../assets/background.png');
     // new sprite
-    var background = new PIXI.Sprite(bgTexture);
+    let background = new PIXI.Sprite(bgTexture);
     app.stage.addChild(background);
-    
+
 
     // Do that responsive design...
     let onResize = (event: any = null) => {
@@ -58,9 +61,34 @@ function viewMain(){
     window.addEventListener('resize', onResize);
 }
 
+function showMessageWindow(){
+    // draw a rounded rectangle
+    let messageWindow = new PIXI.Graphics();
+    app.stage.addChild(messageWindow);
+
+    let style = {font:'bold 24pt Arial', fill:'white'};
+    message = new PIXI.Text("", style);
+    messageWindow.addChild(message);
+
+    // Do that responsive design...
+    let onResize = (event: any = null) => {
+        messageWindow.clear();
+        messageWindow.lineStyle(4, 0xFFFFFF, 1);
+        messageWindow.beginFill(0x0000FF, 0.4);
+        messageWindow.drawRoundedRect(width * 0.15, height * 0.7, width * 0.7, height * 0.25, 15);
+        messageWindow.endFill();
+
+        message.position.x = width * 0.15 + 10;
+        message.position.y = height * 0.7 + 5;
+        message.style.fontSize = width * 0.035;
+    };
+    onResize();
+    window.addEventListener('resize', onResize);
+    
+}
+
 // Load cubism assets.
 function loadAssets() {
-
     // Load one cubism model per one promise.
     var p1 = new Promise((resolve, reject) => {
         new PIXI.loaders.Loader()
@@ -80,6 +108,10 @@ function loadAssets() {
                 koharu.animator.addLayer("base", LIVE2DCUBISMFRAMEWORK.BuiltinAnimationBlenders.OVERRIDE, 1);
                 // Play animation.
                 koharu.animator.getLayer("base").play(animation);
+                // Add animation callback with userdata value.
+                animation.addAnimationCallback((value: string) => {
+                    message.text = value;
+                })
 
                 resolve();
             });
@@ -87,68 +119,29 @@ function loadAssets() {
         });
     });
 
-    // Load one cubism model per one promise.
-    var p2 = new Promise((resolve, reject) => { 
-        new PIXI.loaders.Loader()
-        .add('model3', "../assets/Mark/Mark.model3.json", { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON })
-        .add('motion', "../assets/Mark/Mark.motion3.json", { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON })
-        .load((loader: PIXI.loaders.Loader, resources: PIXI.loaders.ResourceDictionary) => {
-            // Create model.
-            new LIVE2DCUBISMPIXI.ModelBuilder().buildFromModel3Json(loader, resources['model3'], (model: LIVE2DCUBISMPIXI.Model) => {
-                if(model == null)
-                    reject();
-
-                // Set Model instance.
-                mark = model;
-                // Load animation.
-                let animation = LIVE2DCUBISMFRAMEWORK.Animation.fromMotion3Json(loader.resources['motion'].data);
-                // Add animation layer.
-                mark.animator.addLayer("base", LIVE2DCUBISMFRAMEWORK.BuiltinAnimationBlenders.OVERRIDE, 1);
-                // Play animation.
-                mark.animator.getLayer("base").play(animation);
-
-                resolve();
-            });
-        }); 
-    });
-
     // When all promise ended, will return.
-    return Promise.all([p1, p2]);
+    return Promise.all([p1]);
 }
 
 
 function addModel() {
-
     // Add model to stage.
     app.stage.addChild(koharu);
     app.stage.addChild(koharu.masks);
-
-    // Add model to stage.
-    app.stage.addChild(mark);
-    app.stage.addChild(mark.masks);
 
     // Set up ticker.
     app.ticker.add((deltaTime) => {
         koharu.update(deltaTime);
         koharu.masks.update(app.renderer);
-        
-        mark.update(deltaTime);
-        mark.masks.update(app.renderer);
     });
     
     // Do that responsive design...
     let onResize = (event: any = null) => {
         // Resize model.
-        koharu.position = new PIXI.Point((width * 0.25), (height * 0.5));
+        koharu.position = new PIXI.Point((width * 0.5), (height * 0.5));
         koharu.scale = new PIXI.Point((width * 0.4), (width * 0.4));
         // Resize mask texture.
         koharu.masks.resize(app.view.width, app.view.height);
-
-        // Resize model.
-        mark.position = new PIXI.Point((width * 0.75), (height * 0.5));
-        mark.scale = new PIXI.Point((width * 0.4), (width * 0.4));
-        // Resize mask texture.
-        mark.masks.resize(app.view.width, app.view.height);
     };
     onResize();
     window.addEventListener('resize', onResize);
--- example/wwwroot/js/pixiuserdata.js
+++ example/wwwroot/js/pixiuserdata.js
@@ -1,13 +1,14 @@
-var piximultiplemodel;
-(function (piximultiplemodel) {
+var pixiuserdata;
+(function (pixiuserdata) {
     var app;
     var koharu;
-    var mark;
+    var message;
     var width = window.innerWidth;
     var height = (width / 16.0) * 9.0;
     viewMain();
     loadAssets()
         .then(addModel)
+        .then(showMessageWindow)
         .catch(loadFail);
     function viewMain() {
         var canvas = document.getElementById('canvas');
@@ -29,6 +30,26 @@ var piximultiplemodel;
         onResize();
         window.addEventListener('resize', onResize);
     }
+    function showMessageWindow() {
+        var messageWindow = new PIXI.Graphics();
+        app.stage.addChild(messageWindow);
+        var style = { font: 'bold 24pt Arial', fill: 'white' };
+        message = new PIXI.Text("", style);
+        messageWindow.addChild(message);
+        var onResize = function (event) {
+            if (event === void 0) { event = null; }
+            messageWindow.clear();
+            messageWindow.lineStyle(4, 0xFFFFFF, 1);
+            messageWindow.beginFill(0x0000FF, 0.4);
+            messageWindow.drawRoundedRect(width * 0.15, height * 0.7, width * 0.7, height * 0.25, 15);
+            messageWindow.endFill();
+            message.position.x = width * 0.15 + 10;
+            message.position.y = height * 0.7 + 5;
+            message.style.fontSize = width * 0.035;
+        };
+        onResize();
+        window.addEventListener('resize', onResize);
+    }
     function loadAssets() {
         var p1 = new Promise(function (resolve, reject) {
             new PIXI.loaders.Loader()
@@ -42,47 +63,27 @@ var piximultiplemodel;
                     var animation = LIVE2DCUBISMFRAMEWORK.Animation.fromMotion3Json(loader.resources['motion'].data);
                     koharu.animator.addLayer("base", LIVE2DCUBISMFRAMEWORK.BuiltinAnimationBlenders.OVERRIDE, 1);
                     koharu.animator.getLayer("base").play(animation);
+                    animation.addAnimationCallback(function (value) {
+                        message.text = value;
+                    });
                     resolve();
                 });
             });
         });
-        var p2 = new Promise(function (resolve, reject) {
-            new PIXI.loaders.Loader()
-                .add('model3', "../assets/Mark/Mark.model3.json", { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON })
-                .add('motion', "../assets/Mark/Mark.motion3.json", { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON })
-                .load(function (loader, resources) {
-                new LIVE2DCUBISMPIXI.ModelBuilder().buildFromModel3Json(loader, resources['model3'], function (model) {
-                    if (model == null)
-                        reject();
-                    mark = model;
-                    var animation = LIVE2DCUBISMFRAMEWORK.Animation.fromMotion3Json(loader.resources['motion'].data);
-                    mark.animator.addLayer("base", LIVE2DCUBISMFRAMEWORK.BuiltinAnimationBlenders.OVERRIDE, 1);
-                    mark.animator.getLayer("base").play(animation);
-                    resolve();
-                });
-            });
-        });
-        return Promise.all([p1, p2]);
+        return Promise.all([p1]);
     }
     function addModel() {
         app.stage.addChild(koharu);
         app.stage.addChild(koharu.masks);
-        app.stage.addChild(mark);
-        app.stage.addChild(mark.masks);
         app.ticker.add(function (deltaTime) {
             koharu.update(deltaTime);
             koharu.masks.update(app.renderer);
-            mark.update(deltaTime);
-            mark.masks.update(app.renderer);
         });
         var onResize = function (event) {
             if (event === void 0) { event = null; }
-            koharu.position = new PIXI.Point((width * 0.25), (height * 0.5));
+            koharu.position = new PIXI.Point((width * 0.5), (height * 0.5));
             koharu.scale = new PIXI.Point((width * 0.4), (width * 0.4));
             koharu.masks.resize(app.view.width, app.view.height);
-            mark.position = new PIXI.Point((width * 0.75), (height * 0.5));
-            mark.scale = new PIXI.Point((width * 0.4), (width * 0.4));
-            mark.masks.resize(app.view.width, app.view.height);
         };
         onResize();
         window.addEventListener('resize', onResize);
@@ -90,4 +91,4 @@ var piximultiplemodel;
     function loadFail() {
         console.log("Load Fail");
     }
-})(piximultiplemodel || (piximultiplemodel = {}));
+})(pixiuserdata || (pixiuserdata = {}));
